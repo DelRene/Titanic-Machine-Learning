4c505df3fd4f0da37226a8291b86dad999ab03cd
Del Rene <r.delanne@gmail.com>
2023-11-16 16:14:44 +0100

    Mon premier commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use machine learning to create a model that predicts which passengers survived the Titanic shipwreck.',
+    author='Del Rene',
+    license='',
+)
